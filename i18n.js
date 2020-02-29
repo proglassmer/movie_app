01d1076157297fce374293c47834d9d39f1decb3
priyakorn thientumrongsuk <priyakorn.tang@gmail.com>
2020-03-01 01:19:10 +0700

    unuse i18n

--- i18n.js
+++ i18n.js
@@ -1,17 +1,4 @@
 const NextI18Next = require('next-i18next').default
-// const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
-
-const localeSubpathVariations = {
-  none: {},
-  foreign: {
-    th: 'th',
-  },
-  all: {
-    en: 'en',
-    th: 'th',
-  },
-}
-
 module.exports = new NextI18Next({
 	defaultLanguage: 'th',
   otherLanguages: ['en'],
